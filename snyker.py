38d162010189125012dfa836c0dfbd65474cf628
TomasS <tomas.sjekel@26house.com>
2021-05-30 15:06:16 +0200

    Ability to change packages

--- snyker.py
+++ snyker.py
@@ -9,16 +9,8 @@ import time
 service_key = config.api_key
 openai.api_key = service_key
 
-# PACKAGE NAME
-pkg_name = input("Enter package name: ")
 
-# Get the URL
-URL = 'https://snyk.io/advisor/npm-package/' + pkg_name
-URL_dep = URL + '#dependencies'
-r =requests.get(URL)
-r_dep =requests.get(URL_dep)
-
-def handle_answer(q):
+def handle_answer(q, file_id):
 	try:
 		answer = openai.Answer.create(
 			search_model="davinci", 
@@ -37,79 +29,95 @@ def handle_answer(q):
 	
 	return answer
 	
-
-if r.status_code == 200:
-
-	soup = bs(r.text, "html.parser")
-	soup_dep = bs(r_dep.text, "html.parser")
-
-	# Parse the HTML for the data
-	data_set = []
-	dependencies = ['List of package dependencies']
+def handle_package(pkg_name):
+	# Get the URL
+	URL = 'https://snyk.io/advisor/npm-package/' + pkg_name
+	URL_dep = URL + '#dependencies'
+	r = requests.get(URL)
+	r_dep = requests.get(URL_dep)
 	
-	for tag in soup.find_all('div', class_='intro'):
-		for ele in tag.find_all('h2'):
-			msg = f"{pkg_name} is {ele.text}"
-			data_set.append(msg)
-	for tag in soup.find_all('div', class_='number'):
-		data_set.append(tag.text)
-	for tag in soup.find_all('ul', class_='scores'):
-		for ele in tag.find_all('li'):
-		    data_set.append(ele.text)
-	for tag in soup.find_all('dl', class_='stats'):
-		for ele in tag.find_all('div'):
-		    text = ele.text.replace('\n', '')
-		    text = " ".join(text.split())
-		    data_set.append(text)
-	# Readme
-	for tag in soup.find('div', {'id': 'readme'}):
-		text = tag.text.replace('\n', '')
-		text = " ".join(text.split())
-		data_set.append(text)
-	# Dependencies
-	for tag in soup_dep.find_all('div', {'id': 'dependencies'}):
-		for ele in tag.find_all('a'):
-	#         print(ele.text)
-		    dependencies.append(ele.text)
-		    
-	# Create list of JSON objects
-	list_of_jsons = []
-	for line in data_set:
-		json_obj = {"text": line}
-		list_of_jsons.append(json_obj)
-	dep = ", ".join(dependencies)
-	dep_obj = [{'text' : dep, "metadata": "list of package dependencies"}]
+	if r.status_code == 200:
 
-	# Create file with data
-	with open('data.jsonl', 'w') as json_file:
-		for ele in list_of_jsons:
-		    json.dump(ele, json_file)
-		    json_file.write('\n')
-		for ele in dep_obj:
-		    json.dump(ele, json_file)
-		    json_file.write('\n')
-		    
-	# Send file to openAI
-	file = openai.File.create(file=open("data.jsonl"), purpose='answers')
-	time.sleep(3)
-	file_id = file['id']
+		soup = bs(r.text, "html.parser")
+		soup_dep = bs(r_dep.text, "html.parser")
 
-	print("--------------------")
-	print("Type 'exit' to quit.")
-	print("--------------------")
-	# QUESTION:
-	q = input("Question: ")
+		# Parse the HTML for the data
+		data_set = []
+		dependencies = ['List of package dependencies']
 
-	while q != 'exit':
-		# Get an answer
-		answer = handle_answer(q)
+		for tag in soup.find_all('div', class_='intro'):
+			for ele in tag.find_all('h2'):
+				msg = f"{pkg_name} is {ele.text}"
+				data_set.append(msg)
+		for tag in soup.find_all('div', class_='number'):
+			data_set.append(tag.text)
+		for tag in soup.find_all('ul', class_='scores'):
+			for ele in tag.find_all('li'):
+				data_set.append(ele.text)
+		for tag in soup.find_all('dl', class_='stats'):
+			for ele in tag.find_all('div'):
+				text = ele.text.replace('\n', '')
+				text = " ".join(text.split())
+				data_set.append(text)
+		# Readme
+		for tag in soup.find('div', {'id': 'readme'}):
+			text = tag.text.replace('\n', '')
+			text = " ".join(text.split())
+			data_set.append(text)
+		# Dependencies
+		for tag in soup_dep.find_all('div', {'id': 'dependencies'}):
+			for ele in tag.find_all('a'):
+		#         print(ele.text)
+				dependencies.append(ele.text)
+				
+		# Create list of JSON objects
+		list_of_jsons = []
+		for line in data_set:
+			json_obj = {"text": line}
+			list_of_jsons.append(json_obj)
+		dep = ", ".join(dependencies)
+		dep_obj = [{'text' : dep, "metadata": "list of package dependencies"}]
+
+		# Create file with data
+		with open('data.jsonl', 'w') as json_file:
+			for ele in list_of_jsons:
+				json.dump(ele, json_file)
+				json_file.write('\n')
+			for ele in dep_obj:
+				json.dump(ele, json_file)
+				json_file.write('\n')
+		print(data_set)
+		print(list_of_jsons)
+		# Send file to openAI
+		file = openai.File.create(file=open("data.jsonl"), purpose='answers')
+		time.sleep(3)
+		file_id = file['id']
+		return file_id
+	else:
+		print("Package not found")
 		
-		if answer:
-			ans = "".join(answer['answers'])
-			print(f"A: {ans}")
-			q = input("Question: ")
-		else:
-			print("Couldn't find an answer.")
-			q = input("Question: ")
-else:
-	print("Package not found.")
+# PACKAGE NAME
+pkg_name = input("Enter package name: ")
+file_id = handle_package(pkg_name)
+print("--------------------")
+print("Type 'exit' to quit.")
+print("Type 'pkg' to change package.")
+print("--------------------")
+# QUESTION:
+q = input("Question: ")
+
+while q != 'exit':
+	if q == 'pkg':
+		pkg_name = input("Enter package name: ")
+		file_id = handle_package(pkg_name)
+	# Get an answer
+	answer = handle_answer(q, file_id)
+	
+	if answer:
+		ans = "".join(answer['answers'])
+		print(f"A: {ans}")
+		q = input("Question: ")
+	else:
+		print("Couldn't find an answer.")
+		q = input("Question: ")
+
